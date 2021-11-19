ca8ac063b31a3a3e2d7e879402f0016f8311c54b
Josh Hehir <hehir24@gmail.com>
2021-11-19 13:19:08 +1100

    New files
    Added new project and got NUnit setup

---
+++ CoduranceKata/CoduranceKata/Message.cs
@@ -0,0 +1,28 @@
+﻿/*
+* File: Message.cs
+* Author: Josh Hehir 
+* Date: 19/11/2021
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoduranceKata
+{
+    public class Message
+    {
+        private string text;
+        public Message(string text)
+        {
+            this.text = text;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
